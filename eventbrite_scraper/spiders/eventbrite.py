721df2970af2a410d70c2e032a3e294510f860c0
David Grant <davidgrant@gmail.com>
2019-09-20 14:34:42 -0700

    first commit

---
+++ eventbrite_scraper/spiders/eventbrite.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "eventbrite"
+    allowed_domains = ['eventbrite.ca']
+    start_urls = ['https://www.eventbrite.ca/e/raina-telgemeier-tickets-68295733377']
+
+#    def start_requests(self):
+#        urls = [
+#            'https://www.eventbrite.ca/e/raina-telgemeier-tickets-68295733377',
+#        ]
+#        for url in urls:
+#            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        self.log("Got body:" + response.body.decode())
+        sold_out_text = response.xpath('//div[@data-automation = "micro-ticket-box-status"]/text()').get().strip()
+        if sold_out_text != "Sold Out":
+            self.log("RESULT: Tickets available!")
+        else:
+            self.log("RESULT: Sold out!")
